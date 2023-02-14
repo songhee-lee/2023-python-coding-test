5fd78534c5a3e08cf3e92760ad01f786486e63c6
Songhee Lee <songhee172@gmail.com>
2023-02-14 14:33:42 +0900

    Update 곱하기 혹은 더하기.py

--- 1. Greedy/songhee/곱하기 혹은 더하기.py
+++ 1. Greedy/songhee/곱하기 혹은 더하기.py
@@ -10,7 +10,7 @@ s = [ int(x) for x in input() ]
 # 2. 계산
 result = s[0]    
 for i in s[1:] :      
-    if result <= 1 or i <= 0 :    # 1보다 작으면 더하기
+    if result <= 1 or i <= 1 :    # 1보다 작으면 더하기
         result += i
     else:                         # 아니면 곱하기
         result *= i
