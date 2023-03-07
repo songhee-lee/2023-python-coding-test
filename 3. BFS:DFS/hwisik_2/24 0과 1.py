45ec724f238d0f4b87d2fcbf7bf8bb57dd3017e5
lunarmoon7 <pisik05@konkuk.ac.kr>
2023-03-07 12:32:36 +0900

    [HS] BFS/DFS 0과 1

--- 3. BFS:DFS/hwisik_2/24 0과 1.py
+++ 3. BFS:DFS/hwisik_2/24 0과 1.py
@@ -6,6 +6,10 @@
 - A mod B = C, 임의의 수 D
 -> (A * D) mod B = (C * D) mod B
 -> (A + D) mod B = (C + D) mod B
+    - ex) 100 mod 8 = 4
+            -> (100 * 10 + 1) mod 8 = 1 
+            -> (4 * 10 + 1) mod 8 = 1
+-> ✅'다시풀기'
 '''
 from collections import deque
 import sys
@@ -33,14 +37,18 @@ def modular_bfs(n):
     
     while queue:
         cur_num, cur_str = queue.popleft()
+        
         if cur_num == 0:
             return cur_str
         
+        # 길이가 100 넘어가면
         if len(cur_str) > 100:
             return 'BARK'
         
+        # 뒤에 0 또는 1을 붙였을 때
         _append_zero = cur_num * 10
         _append_one = cur_num * 10 + 1
+        
         if not visited[_append_zero % n]:
             visited[_append_zero % n] = 1
             queue.append((_append_zero % n, cur_str + '0'))
@@ -48,7 +56,8 @@ def modular_bfs(n):
             visited[_append_one % n] = 1
             queue.append((_append_one % n, cur_str + '1'))
             
-    return 'BARK' 
+    return 'BARK'
+
 t = int(input())
 
 for _ in range(t):
