42b1a365ba9ecca202956df3fa9e10106695be2d
songhee <songhee172@gmail.com>
2023-03-07 12:12:08 +0900

    [SH] BFS/DFS 적록 색약

---
+++ 3. BFS:DFS/songhee/BFS/05. 적록색약 10026.py
@@ -0,0 +1,55 @@
+""" 
+
+"""
+from collections import deque
+
+def bfs(i, j, color):
+    q = deque([(i, j)])
+    visited[i][j] = False
+
+    while q:
+        x, y = q.popleft()
+
+        for i in range(4):
+            nx, ny = x+dx[i], y+dy[i]
+
+            if 0 <= nx < N and 0 <= ny < N and visited[nx][ny]:
+                if maps[nx][ny] in color:
+                    visited[nx][ny] = False
+                    q.append((nx, ny))
+
+                
+# 1. 입력 받기
+N = int(input())
+maps = []
+for _ in range(N):
+    maps.append(list(input()))
+
+# 상하좌우 이동
+dx = (-1, 1, 0, 0)
+dy = (0, 0, -1, 1)
+
+result = { 'R':0, 'G':0, 'B':0}
+visited = [[True]*N for _ in range(N)]
+for i in range(N):
+    for j in range(N):
+        if visited[i][j] :
+            bfs(i, j, maps[i][j])
+            result[maps[i][j]] += 1
+
+r, g, b = result['R'], result['G'], result['B']
+print(r+g+b, end=' ')
+
+result = [0, 0]
+visited = [[True]*N for _ in range(N)]
+for i in range(N):
+    for j in range(N):
+        if visited[i][j]:
+            color = maps[i][j]
+            if color == 'B':
+                bfs(i, j, 'B')
+                result[1] += 1
+            else:
+                bfs(i, j, {'R', 'G'})
+                result[0] += 1
+print(sum(result))
