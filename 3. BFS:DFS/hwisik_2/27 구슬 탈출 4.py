bc928f4b80da5d08fab71a0a45e68f6a94c68194
lunarmoon7 <pisik05@konkuk.ac.kr>
2023-03-13 19:23:06 +0900

    [HS] BFS/DFS - 구슬 탈출 4

--- 3. BFS:DFS/hwisik_2/27 구슬 탈출 4.py
+++ 3. BFS:DFS/hwisik_2/27 구슬 탈출 4.py
@@ -6,79 +6,84 @@
 '''
 
 from collections import deque
+from pprint import pprint
 
-# 보드 기울이기
+# 보드 기울이기 - 구슬의 위치 (x, y), 기울일 방향 (dx, dy)
 def tilt_graph(x, y, dx, dy):
-    dist = 0 # 기울였을 때 이동한 거리
+    move_time = 0 # 보드 기울였을 때, 총 이동 횟수
     while True:
-        if graph[x][y] == 'O': # 구멍에 빠지면
+        if graph[x][y] == 'O': # 구멍이면
             break
         if graph[x][y] == '#': # 벽이라면
-            # 뒤로 이동 후, 종료
+            
+            # 뒤로 물러난다.
             x -= dx 
             y -= dy
             break
         
+        # 위치 갱신
         x += dx
         y += dy
-        dist += 1
-    return (x, y, dist)
+        
+        # 이동 횟수 + 1
+        move_time += 1
+        
+    return (x, y, move_time)
 
-# BFS - (rx, ry) : 빨간 구슬 위치, (bx, by) : 파란 구슬 위치
+# BFS
 def bfs(rx, ry, bx, by):
     queue = deque([(rx, ry, bx, by, 1)])
-    
     visited = [[[[0 for _ in range(m)] for _ in range(n)] for _ in range(m)] for _ in range(n)]
+    
     visited[rx][ry][bx][by] = 1
     
-    
     while queue:
-        rx, ry, bx, by, move_cnt = queue.popleft()
-        
+        rx, ry, bx, by, move_time = queue.popleft()
         for i in range(4):
-            nrx, nry, dist_red = tilt_graph(rx, ry, dx[i], dy[i]) # 빨간 구슬 이동
-            nbx, nby, dist_blue = tilt_graph(bx, by, dx[i], dy[i]) # 파란 구슬 이동
+            # 기울이기 - 빨간 구슬
+            nrx, nry, red_move_time = tilt_graph(rx, ry, dx[i], dy[i])
             
-            # 파란 구슬이 구멍에 들어가면, 게임 실패
+            # 기울이기 - 파란 구슬
+            nbx, nby, blue_move_time = tilt_graph(bx, by, dx[i], dy[i])
+            
+            # 파란 구슬이 구멍에 빠지면, 실패
             if graph[nbx][nby] == 'O':
                 continue
             
-            # 빨간 구슬이 구멍에 들어가면, 게임 성공
+            # 빨간 구슬이 구멍에 빠지면, 성공
             if graph[nrx][nry] == 'O':
-                return move_cnt # 이동 횟수 반환
+                return move_time
             
-            # 보드를 기울인 후, 
-            # 빨간 구슬과 파란 구슬의 위치가 같다면
+            # 두 구슬의 위치가 같다면,
+            # 문제의 조건에 따라, 두 구슬은 동시에 같은 칸에 있을 수 없다.
             if nrx == nbx and nry == nby:
-                
-                # 파란 구슬이 더 많이 이동 - 파란 구슬이 빨간 구슬보다 더 뒤에 있었다.
-                if dist_red < dist_blue: 
-                    nbx, nby = nbx - dx[i], nby - dy[i] # 파란 구슬 뒤로 이동
-                    
-                # 빨간 구슬이 더 많이 이동 - 빨간 구슬이 파란 구슬보다 더 뒤에 있었다.
+                # 파란 구슬이 빨간 구슬보다 더 뒤에있었음
+                if red_move_time < blue_move_time: 
+                    nbx, nby = nbx - dx[i], nby - dy[i] # 뒤로 물러난다.
+                # 빨간 구슬이 파란 구슬보다 더 뒤에있었음   
                 else:
-                    nrx, nry = nrx - dx[i], nry - dy[i] # 빨간 구슬 뒤로 이동
+                    nrx, nry = nrx - dx[i], nry - dy[i] # 뒤로 물러난다.
             
-            # 방문한 적 없으면
+            # 방문한적 없으면
             if not visited[nrx][nry][nbx][nby]:
                 visited[nrx][nry][nbx][nby] = 1
-                queue.append((nrx, nry, nbx, nby, move_cnt + 1))
+                queue.append((nrx, nry, nbx, nby, move_time + 1))
                 
-    return -1 # 빨간 구슬을 구멍을 통해 빼낼 수 없다.
-            
+    return -1 # 어떻게 움직여도 빨간 구슬을 구멍으로 빼낼 수 없다.
+
 n, m = map(int, input().split())
 
 graph = []
 
-# 보드 이동 방향(상, 하, 좌, 우)
+# 방향 정보(상, 하, 좌, 우)
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-# 빨간 구슬과 파란 구슬의 위치를 구한다.
-# 해당 위치의 보드 값은 '.'(빈 칸)으로 초기화
+# 빨간 구슬의 (x, y), 파란 구슬의 (x, y)
+rx, ry, bx, by = 0, 0, 0, 0 
+
 for i in range(n):
-    input_data = list(input())
-    graph.append(input_data)
+    graph.append(list(input()))
     for j in range(m):
         if graph[i][j] == 'R':
             rx, ry = i, j
@@ -87,8 +92,7 @@ for i in range(n):
             bx, by = i, j
             graph[i][j] = '.'
 
-# BFS 수행
+# 빨간 구슬과 파란 구슬의 위치를 인자로 BFS 수행
 ret = bfs(rx, ry, bx, by)
 
-# 출력
 print(ret)
