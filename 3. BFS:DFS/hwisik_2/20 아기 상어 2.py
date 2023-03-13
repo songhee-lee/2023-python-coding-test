1f0231ef28bafb49b994c6f06114be852194ef0f
lunarmoon7 <pisik05@konkuk.ac.kr>
2023-03-13 19:22:38 +0900

    [HS] BFS/DFS - 아기 상어 2

--- 3. BFS:DFS/hwisik_2/20 아기 상어 2.py
+++ 3. BFS:DFS/hwisik_2/20 아기 상어 2.py
@@ -2,19 +2,25 @@
 - 안전거리 : 그 칸과 가장 거리가 가까운 아기 상어와의 거리 -> ✅'BFS'
 - 상어는 여러 마리가 존재한다. 따라서, 각 상어의 위치마다 안전거리를 구해서 갱신해야 한다.
 - 여기서는 '거리 배열이 방문 표시 배열의 역할을 할 수 없다?'
+
+-> '피드백' : 각 상어의 위치마다 BFS를 돌리지 않고, 큐에 한번에 넣으면 된다. -> 72ms (기존 코드 : 2376ms)
 '''
 from collections import deque
 
 # BFS
-def bfs(x, y):
+def bfs():
     # 각 상어의 위치마다 안전거리를 구해야 하므로 
     # 방문 표시 배열을 BFS안에 선언
-    visited = [[0] * m for _ in range(n)]
-    queue = deque([(x, y)])
+    queue = deque()
     
-    # 현재 상어가 위치한 곳의 안전 거리 = 0, 방문 표시
-    dist[x][y] = 0 
-    visited[x][y] = 1
+    for i in range(n):
+        for j in range(m):
+            if graph[i][j] == 1:
+                queue.append((i, j))
+                
+                # 현재 상어가 위치한 곳의 안전 거리 = 0, 방문 표시
+                dist[i][j] = 0 
+                visited[i][j] = 1
     
     while queue:
         x, y = queue.popleft()
@@ -44,15 +50,13 @@ for _ in range(n):
 INF = float('inf')
 
 dist = [[INF] * m for _ in range(n)] # 각 칸의 안전 거리의 최솟값 
+visited = [[0] * m for _ in range(n)]
 
 safe_dist = 0 # 안전 거리
 
-# 각 상어마다 BFS 수행
-for i in range(n):
-    for j in range(m):
-        if graph[i][j] == 1:
-            bfs(i, j)
-            
+# BFS
+bfs()
+
 # 안전거리의 최댓값 구하기
 for i in range(n):
     safe_dist = max(safe_dist, max(dist[i]))
