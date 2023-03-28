58892ea3de4dd53cab86313793fb29a9eb91a69a
lunarmoon7 <pisik05@konkuk.ac.kr>
2023-03-28 13:04:21 +0900

    [HS] Binary Search - (modify Comments)

--- 5. Binary Search/Hwisik/23. 구간 나누기 2.py
+++ 5. Binary Search/Hwisik/23. 구간 나누기 2.py
@@ -1,7 +1,12 @@
 '''
-- M개 이하의 구간을 나눈다.
+[설명]
+- N개의 수로 이루어진 1차원 배열이 있다. 이 배열을 M개 이하의 구간으로 나누어서, 구간의 점수의 최댓값을 최소로 하려고 한다.
+    - 하나의 구간은 하나 이상의 연속된 수들로 이루어져 있다.
+    - 뱁열의 각 수는 모두 하나의 구간에 포함되어 있어야 한다.
 - 구간의 점수 : 최대값 - 최솟값
-- 어떤 것을 mid로 설정?
+
+[아이디어]
+- 이진탐색을 수행하는데 어떤 것을 mid로 설정할 것인가?
 
 -> ✅ 다시풀기
 '''
@@ -9,41 +14,47 @@
 import sys
 
 # 구간 나누기
-def get_divided_count(mid):
-    min_val = arr[0] # 구간에서 가장 작은 값
-    max_val = arr[0] # 구간에서 가장 큰 값
-    count = 1 # 구간의 개수
-    
+def split_sector(mid):
+    _min = arr[0]  # 구간에서 가장 작은 값
+    _max = arr[0]  # 구간에서 가장 큰 값
+    sector_count = 1  # 구간의 개수
+
     for i in range(1, n):
-        max_val = max(max_val, arr[i])
-        min_val = min(min_val, arr[i])
-        
+        _min = min(_min, arr[i])
+        _max = max(_max, arr[i])
+
         # 구간의 점수가 기준값(mid)보다 크면
-        # 구간을 나눌 수 있다.
-        if max_val - min_val > mid: 
-            count += 1 # 구간의 개수 + 1
-            max_val = arr[i]
-            min_val = arr[i]
-        
-    return count # 구간의 개수 반환
+        # 구간을 나눌 수 있다
+        if _max - _min > mid:
+            sector_count += 1
+            _min = arr[i]
+            _max = arr[i]
+
+    return sector_count  # 구간의 개수 반환
+
+
+# 이진 탐색
+def binary_search():
+    l, r = 0, max(arr)
+    ret = 0
+    while l <= r:
+        mid = l + (r - l) // 2  # 각 구간의 점수의 최댓값 중 최솟값
+
+        # 구간의 개수가 m과 같거나 작다면 => 구간의 점수가 너무 클 때
+        if split_sector(mid) <= m:
+            ret = mid
+            r = mid - 1  # 구간의 점수 줄인다.(=구간의 개수 늘린다), 왼쪽 부분 탐색
+
+        # 구간의 개수가 m보다 크다면 => 구간의 점수가 너무 작을 때
+        else:
+            l = mid + 1  # 구간의 점수 늘린다.(=구간의 개수 줄인다), 오른쪽 부분 탐색
+
+    return ret
+
 
 n, m = map(int, sys.stdin.readline().split())
 arr = list(map(int, sys.stdin.readline().split()))
 
-l, r = 0, max(arr)
-ret = 0
+ret = binary_search()
 
-# 이진 탐색
-while l <= r:
-    mid = l + (r - l) // 2 # 각 구간의 점수의 최댓값 중 최솟값
-    
-    # 구간의 개수가 m과 같거나 작다면 => 구간의 점수가 너무 클 때
-    if get_divided_count(mid) <= m: 
-        ret = mid
-        r = mid - 1 # 구간의 점수 줄인다.(=구간의 개수 늘린다), 왼쪽 부분 탐색
-        
-    # 구간의 개수가 m보다 크다면 => 구간의 점수가 너무 작을 때
-    else:
-        l = mid + 1 # 구간의 점수 늘린다.(=구간의 개수 줄인다), 오른쪽 부분 탐색
-        
 print(ret)
--- 5. Binary Search/Hwisik/29. 선분과 점.py
+++ 5. Binary Search/Hwisik/29. 선분과 점.py
@@ -10,12 +10,15 @@
 
 import sys
 
+# 점 a와 점 b에 대한 이진탐색
 def binary_search(ax, ay, az, bx, by, bz):
     ret = float('inf')
     
     while True:
+        # 점 m : 점 a와 점 b의 중간점
         mx, my, mz = (ax + bx) / 2, (ay + by) / 2, (az + bz) / 2
         
+        # a - c, b - c, m - c 거리 구하기
         a_to_c = calc(ax, ay, az, cx, cy, cz)
         b_to_c = calc(bx, by, bz, cx, cy, cz)
         m_to_c = calc(mx, my, mz, cx, cy, cz)
@@ -25,17 +28,26 @@ def binary_search(ax, ay, az, bx, by, bz):
         
         ret = min(ret, m_to_c)
         
+        # a - c가 b - c 보다 크다면
+        # 점 c가 점 b에 가까이 있다.
         if a_to_c > b_to_c:
             ax, ay, az = mx, my, mz
+            
+        # 점 c가 점 a에 가까이 있다.
         else:
             bx, by, bz = mx, my, mz
             
     return ret
+
+
+# 두 점 사이의 거리 계산
 def calc(x1, y1, z1, x2, y2, z2):
     return ((x1 - x2) ** 2 + (y1 - y2) ** 2 + (z1 - z2) ** 2) ** 0.5    
 
+# 입력
 ax, ay, az, bx, by, bz, cx, cy, cz = map(int, sys.stdin.readline().split())
 
+# 이진 탐색 수행
 ret = binary_search(ax, ay, az, bx, by, bz)
 
 print('%0.10f' %ret)
