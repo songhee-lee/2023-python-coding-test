e68c89dba496ff059ebc43655c6cdcc0391c877c
Songhee Lee <songhee172@gmail.com>
2023-04-11 16:14:34 +0900

    [SH] DP - 변수명 정리

--- 6. DP/songhee/21. 파일 합치기.py
+++ 6. DP/songhee/21. 파일 합치기.py
@@ -16,9 +16,10 @@ for _ in range(int(input())):
     
     # dp[i][j] : i~j까지 합치는데 필요한 최소 비용
     dp = [ [0]*(K+1) for _ in range(K+1)]
-    for i in range(2, K+1):         # 파일의 거리
-        for j in range(1, K+2-i):   # 시작점
+    for distance in range(2, K+1):         # 파일의 거리
+        for start in range(1, K+2-distance):   # 시작점
             # dp[a][m] = dp[m+1][b] + sum(f[a]~f[b])
-            dp[j][j+i-1] = min([dp[j][j+k]+dp[j+k+1][j+i-1] for k in range(i-1)]) + ( files[j+i-1] - files[j-1])
+            end = start + distance -1
+            dp[start][end] = min([dp[start][start+cut]+dp[start+cut+1][end] for cut in range(distance-1)]) + ( files[end] - files[start-1])
     
     print(dp[1][K])
