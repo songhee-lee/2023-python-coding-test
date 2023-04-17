d30ce14b64ec20e94a035eb6f4d12dec2431b633
lunarmoon7 <pisik05@konkuk.ac.kr>
2023-04-17 12:04:32 +0900

    [HS] Shortest Path - 화성 탐사

--- 7. Shortest Path/Hwisik/05. 화성 탐사.py
+++ 7. Shortest Path/Hwisik/05. 화성 탐사.py
@@ -1,5 +1,6 @@
 import sys, heapq
 
+# 개선된 다익스트라 알고리즘
 def dijkstra(x, y):
     q = []
     heapq.heappush(q, (graph[x][y], x, y))
@@ -14,15 +15,16 @@ def dijkstra(x, y):
         for i in range(4):
             nx, ny = x + dx[i], y + dy[i]
             
-            if 0 <= nx < n and 0 <= ny < n:
+            if 0 <= nx < n and 0 <= ny < n: # 범위 내에 있으면
                 cost = d + graph[nx][ny]
                 
-                if cost < dist[nx][ny]:
+                if cost < dist[nx][ny]: # 최단 거리 갱신
                     dist[nx][ny] = cost
                     heapq.heappush(q, (cost, nx, ny))
 
 
 INF = int(1e9)
+# 방향 정보(상, 하, 좌, 우)
 dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
 t = int(sys.stdin.readline())
 
@@ -32,5 +34,5 @@ for _ in range(t):
     dist = [[INF] * n for _ in range(n)]
     
     dijkstra(0, 0)
-    print(dist)
+    
     print(dist[n - 1][n - 1])
