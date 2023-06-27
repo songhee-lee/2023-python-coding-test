c3584d5d3fbf3d30d314619b92ee41a27d031a9f
0sun-creater <54173210+0sun-creater@users.noreply.github.com>
2023-06-27 19:57:40 +0900

    Update 08. 보석쇼핑.py

--- Programmers/08. 보석쇼핑.py
+++ Programmers/08. 보석쇼핑.py
@@ -10,6 +10,39 @@
  “모든 종류의 보석을 포함하는 구간" 여부 : 딕셔너리 자료구조
 '''
 
+def solution(gems):
+    gemSet = set()
+    for gem in gems:
+        if gem not in gemSet:
+            gemSet.add(gem)
+    kinds = len(gemSet)
+    
+    gemDict = {gems[0]: 1}
+    answer = [1, len(gems)]
+    left = 0; right = 0
+    while left <= right and right < len(gems):
+        if len(gemDict) == kinds:
+            if answer[1]-answer[0] > right-left:
+                answer = [left+1, right+1]
+            
+            gemDict[gems[left]] -= 1
+            if gemDict[gems[left]] == 0:
+                del gemDict[gems[left]]
+            left += 1
+
+        elif len(gemDict) < kinds:
+            right += 1
+            if right >= len(gems):
+                break
+                
+            if gems[right] not in gemDict:
+                gemDict[gems[right]] = 1
+            else:
+                gemDict[gems[right]] += 1
+        
+    return answer
+
+'''
 from collections import defaultdict
 
 def solution(gems):
@@ -53,3 +86,4 @@ def solution(gems):
     
     
     return real_answer
+'''
